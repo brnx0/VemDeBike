fd6976dd10c37ccccdade9a6b630c5625e6f3994
Breno <breno6@ba.estudante.senai.br>
2023-01-11 13:51:46 -0300

    Fix Scanner

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -1,46 +1,62 @@
-import React, {useState} from 'react';
-import { Camera} from 'expo-camera';
-
-import { View,  StyleSheet,} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Camera, CameraType} from 'expo-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
 
 
 export default function Scanner(){
 
+  const [type, setType] = useState(CameraType.back);
   const [scanned, setScanned] = useState(false);
-  const [hasPermission, setHasPermission] = useState(null);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  if (hasPermission === null) {
-    setHasPermission ('granted');
-   }
-     
-     
-  
-  if (hasPermission === false) {
-    setHasPermission ('granted');
-  };
-  
- const handleBarCodeScanned = ({type, data}) =>{
-    alert(`${type}, ${data} `);
-    return setScanned(false);
-  };
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
 
-      
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Você precisa conceder permissão a camera</Text>
+        <Button onPress={requestPermission} title="Conceder Permissão" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+  function limpar (){
+   alert('ok')
+  }
+  const handleBarCodeScanned = ({type, data}) =>{
+    alert(`${type}, ${data}, ${scanned} `);
+    return setScanned(true)
+  
+    
+  }
   return (
     <View style={styles.container}>
-      <Camera>
-  
+      <Camera style={styles.camera} type={type}  barCodeScannerSettings={{
+    barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],}}onBarCodeScanned={scanned ? limpar : handleBarCodeScanned}>
+       
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Girar Camera</Text>
+          </TouchableOpacity>
+        </View>
       </Camera>
     </View>
-  )
+  );
 }
 
-
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent:'center'
-  }, 
+    justifyContent: 'center',
+  },
   camera: {
     flex: 1,
   },
@@ -60,4 +76,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
-})
+});
